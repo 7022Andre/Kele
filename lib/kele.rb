ba39ca9eb613ced12256f029eb46d168caea7a5a
7022Andre <7022Andre@users.noreply.github.com>
2017-06-26 13:02:37 -0700

    User story 3 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,4 +16,10 @@ class Kele
 		response = self.class.get("/users/me", headers: { "authorization": @auth_token })
 		@user_data = JSON.parse(response.body)
 	end
+
+	def get_mentor_availability
+		mentor_id = get_me["current_enrollment"]["mentor_id"]
+		response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization": @auth_token })
+		@mentor_avail = JSON.parse(response.body)
+	end
 end
