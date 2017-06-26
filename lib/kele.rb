9aca639e8b7e705e4662fcff2e1996bbee226d2f
7022Andre <7022Andre@users.noreply.github.com>
2017-06-26 11:38:43 -0700

    Update user story 1 solution, rewrite code

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,10 +2,10 @@ require 'httparty'
 
 class Kele
 	include HTTParty
+	base_uri "https://www.bloc.io/api/v1"
 
 	def initialize(email, password)
-		base_uri = "https://www.bloc.io/api/v1"
-		response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+		response = self.class.post("/sessions", body: {"email": email, "password": password})
 		@auth_token = response["auth_token"]
 		@error_message = response["message"]
 	end
