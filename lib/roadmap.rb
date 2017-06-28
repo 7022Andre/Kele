d0a5f06d9ec2a1de02c9d25528e40b6f708a49eb
7022Andre <7022Andre@users.noreply.github.com>
2017-06-28 12:08:51 -0700

    Test Submission for user story (update checkpoint submission)

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -21,4 +21,17 @@ module Roadmap
 				"comment": comment
 			})
 	end
+
+	def update_submission(id, checkpoint_id, assignment_branch, assignment_commit_link, comment)
+		enrollment_id = get_me["current_enrollment"]["id"]
+		response = self.class.put("/checkpoint_submissions/:#{id}",
+			headers: { "authorization": @auth_token	},
+			body: {
+				"enrollment_id": enrollment_id,
+				"checkpoint_id": checkpoint_id,
+				"assignment_branch": assignment_branch,
+				"assignment_commit_link": assignment_commit_link,
+				"comment": comment
+			})
+	end
 end
